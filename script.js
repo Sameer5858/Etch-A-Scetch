7e536811c29aa3546a40a3675f6080caaf7d9040
sameer5858 <www.md.sameerkhan5858@gmail.com>
2022-01-03 15:05:16 +0530

    add done

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const container = document.getElementById("container");
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid");
-  container.appendChild(div);
-}
-
-container.addEventListener("mouseover", (e) => {
-  e.target.classList.add("hov");
-});
 const clearBtn = document.querySelector("button");
+
+let size = 16;
+makeGrid(size);
 clearBtn.addEventListener("click", () => {
+  size = prompt(
+    "how many grids for each side? please enter a number between 0-100",
+    16
+  );
+  if (size < 0) {
+    return alert("please enter a number between 0-100");
+  } else if (size > 100) {
+    return alert("please enter a number between 0-100");
+  } else {
+    removeHov();
+    removeGrid();
+    removeGrid();
+    makeGrid(size);
+  }
+});
+
+function makeGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    container.appendChild(div);
+  }
+}
+function removeGrid() {
+  document.querySelectorAll(".grid").forEach((grid) => {
+    grid.remove();
+  });
+}
+function removeHov() {
   const hovs = document.querySelectorAll(".hov");
   hovs.forEach((hov) => {
     hov.classList.remove("hov");
   });
+}
+container.addEventListener("mouseover", (e) => {
+  e.target.classList.add("hov");
 });
